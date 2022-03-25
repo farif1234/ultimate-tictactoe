bfc1ee3f05766c664234121f009734de9f6d95d6
farif1234 <88285952+farif1234@users.noreply.github.com>
2022-03-25 16:14:04 -0400

    Uploaded all files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Game from './components/Game'
+
+
+
+function App() {
+  return (
+    <>
+      <h1 className='status'>Ultimate Tic Tac Toe! </h1>
+      <Game />
+    </>
+  );
+}
+
+export default App;
